c07c5970a67036f5d1571b796308a37be7bdd4ff
pvp <pvpindian@gmail.com>
2022-09-28 08:24:54 +0530

    latest

--- src/App.js
+++ src/App.js
@@ -1,37 +1,9 @@
-import CategoryItem from './components/category-item/category-item.component';
-
-import './categories.styles.scss';
+import Directory from './components/directory/directory.component';
+import categories from './categories.json'
 
 const App = () => {
-  const categories = [
-    {
-      id: 1,
-      title: 'Hats',
-    },
-    {
-      id: 2,
-      title: 'Jackets',
-    },
-    {
-      id: 3,
-      title: 'Sneakers',
-    },
-    {
-      id: 4,
-      title: 'Womens',
-    },
-    {
-      id: 5,
-      title: 'Mens',
-    },
-  ];
-
   return (
-    <div className='categories-container'>
-      {categories.map((category) => (
-        <CategoryItem key={category.id} category={category} />
-      ))}
-    </div>
+    <Directory categories={categories} />
   );
 };
 
